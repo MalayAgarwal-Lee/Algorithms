33e6f7cfd6077e506d4c48ce048a17b5795ede47
Malay Agarwal <malay.agarwal261016@outlook.com>
2020-01-13 16:44:02 +0530

    Remove create_10_matrices() due to memory problems

--- strassen_mat_mul/strassen_mat_mul.c
+++ strassen_mat_mul/strassen_mat_mul.c
@@ -3,7 +3,6 @@
 #define MAX 2
 
 void input(int A[][MAX], int B[][MAX]);
-void create_10_matrices(int a11[], int a12[], int a21[], int a22[], int* s[], int* a, int* b, int s1, int s2);
 int* multiply(int indices[], int* a, int* b, int s1, int s2);
 void print(int*, int size);
 
@@ -53,91 +52,6 @@ void print(int* mat, int size)
     }
 }
 
-void create_10_matrices(int a11[], int a12[], int a21[], int a22[], int* s[], int* a, int* b, int s1, int s2)
-{
-    int part_size = a11[1] - a11[0] + 1;
-
-    int* mat1 = malloc(sizeof(int) * part_size * part_size);
-
-    for (int i = 0; i < part_size; ++i)
-        for(int j = 0; j < part_size; ++j)
-            *((mat1 + i * part_size) + j) = *((b + (i + a12[0]) * s2) + j + a12[2]) - *((b + (i + a22[0]) * s2) + j + a22[2]);
-
-    s[0] = mat1;
-
-    int* mat2 = malloc(sizeof(int) * part_size * part_size);
-
-    for (int i = 0; i < part_size; ++i)
-        for (int j = 0; j < part_size; ++j)
-            *((mat2 + i * part_size) + j) = *((a + (i + a11[0]) * s1) + j + a11[2]) + *((a + (i + a12[0]) * s1) + j + a12[2]);
-
-    s[1] = mat2;
-
-
-    int* mat3 = (int*)malloc(sizeof(int) * part_size * part_size);
-
-    for (int i = 0; i < part_size; ++i)
-        for (int j = 0; j < part_size; ++j)
-            *((mat3 + i * part_size) + j) = *((a + (i + a21[0]) * s1) + j + a21[2]) + *((a + (i + a22[0]) * s1) + j + a22[2]);
-
-    s[2] = mat3;
-
-    int* mat4 = (int*)malloc(sizeof(int) * part_size * part_size);
-
-    for (int i = 0; i < part_size; ++i)
-        for (int j = 0; j < part_size; ++j)
-            *((mat4 + i * part_size) + j) = *((b + (i + a21[0]) * s2) + j + a21[2]) - *((b + (i + a11[0]) * s2) + j + a11[2]);
-
-    s[3] = mat4;
-
-    int* mat5 = (int*)malloc(sizeof(int) * part_size * part_size);
-
-    for (int i = 0; i < part_size; ++i)
-        for (int j = 0; j < part_size; ++j)
-            *((mat5 + i * part_size) + j) = *((a + (i + a11[0]) * s1) + j + a11[2]) + *((a + (i + a22[0]) * s1) + j + a22[2]);
-
-    s[4] = mat5;
-
-    int* mat6 = (int*)malloc(sizeof(int) * part_size * part_size);
-
-    for (int i = 0; i < part_size; ++i)
-        for (int j = 0; j < part_size; ++j)
-            *((mat6 + i * part_size) + j) = *((b + (i + a11[0]) * s2) + j + a11[2]) + *((b + (i + a22[0]) * s2) + j + a22[2]);
-
-    s[5] = mat6;
-
-    int* mat7 = (int*)malloc(sizeof(int) * part_size * part_size);
-
-    for (int i = 0; i < part_size; ++i)
-        for (int j = 0; j < part_size; ++j)
-            *((mat7 + i * part_size) + j) = *((a + (i + a12[0]) * s1) + j + a12[2]) - *((a + (i + a22[0]) * s1) + j + a22[2]);
-
-    s[6] = mat7;
-
-    int* mat8 = (int*)malloc(sizeof(int) * part_size * part_size);
-
-    for (int i = 0; i < part_size; ++i)
-        for (int j = 0; j < part_size; ++j)
-            *((mat8 + i * part_size) + j) = *((b + (i + a21[0]) * s2) + j + a21[2]) + *((b + (i + a22[0]) * s2) + j + a22[2]);
-
-    s[7] = mat8;
-
-    int* mat9 = (int*)malloc(sizeof(int) * part_size * part_size);
-
-    for (int i = 0; i < part_size; ++i)
-        for (int j = 0; j < part_size; ++j)
-            *((mat9 + i * part_size) + j) = *((a + (i + a11[0]) * s1) + j + a11[2]) - *((a + (i + a21[0]) * s1) + j + a21[2]);
-
-    s[8] = mat9;
-
-    int* mat10 = (int*)malloc(sizeof(int) * part_size * part_size);
-
-    for (int i = 0; i < part_size; ++i)
-        for (int j = 0; j < part_size; ++j)
-            *((mat10 + i * part_size) + j) = *((b + (i + a11[0]) * s2) + j + a11[2]) + *((b + (i + a12[0]) * s2) + j + a12[2]);
-
-    s[9] = mat10;
-}
 
 int* multiply(int indices[], int* a, int* b, int s1, int s2)
 {
@@ -165,21 +79,91 @@ int* multiply(int indices[], int* a, int* b, int s1, int s2)
 
         int a22[] = {a21[0], a21[1], a21[3] + 1, indices[3]};
 
-        int* s[10];
-        create_10_matrices(a11, a12, a21, a22, s, a, b, s1, s2);
+        int* mat = malloc(sizeof(int) * size * size);
 
-        int* p1 = multiply(a11, a, s[0], MAX, size);
-        int* p2 = multiply(a22, s[1], b, size, MAX);
-        int* p3 = multiply(a11, s[2], b, size, MAX);
-        int* p4 = multiply(a22, a, s[3], MAX, size);
+        for (int i = 0; i < size; ++i)
+            for(int j = 0; j < size; ++j)
+                *((mat + i * size) + j) = *((b + (i + a12[0]) * s2) + j + a12[2]) - *((b + (i + a22[0]) * s2) + j + a22[2]);
+
+        int* p1 = multiply(a11, a, mat, MAX, size);
+        free(mat);
+
+        mat = malloc(sizeof(int) * size * size);
+
+        for (int i = 0; i < size; ++i)
+            for (int j = 0; j < size; ++j)
+                *((mat + i * size) + j) = *((a + (i + a11[0]) * s1) + j + a11[2]) + *((a + (i + a12[0]) * s1) + j + a12[2]);
+
+
+        int* p2 = multiply(a22, mat, b, size, MAX);
+        free(mat);
+
+        mat = malloc(sizeof(int) * size * size);
+
+        for (int i = 0; i < size; ++i)
+            for (int j = 0; j < size; ++j)
+                *((mat + i * size) + j) = *((a + (i + a21[0]) * s1) + j + a21[2]) + *((a + (i + a22[0]) * s1) + j + a22[2]);
+
+
+        int* p3 = multiply(a11, mat, b, size, MAX);
+        free(mat);
+
+        mat = malloc(sizeof(int) * size * size);
+
+        for (int i = 0; i < size; ++i)
+            for (int j = 0; j < size; ++j)
+            *((mat + i * size) + j) = *((b + (i + a21[0]) * s2) + j + a21[2]) - *((b + (i + a11[0]) * s2) + j + a11[2]);
+
+        int* p4 = multiply(a22, a, mat, MAX, size);
+        free(mat);
 
         int sindices[] = {0, size - 1, 0, size - 1};
-        int* p5 = multiply(sindices, s[4], s[5], size, size);
-        int* p6 = multiply(sindices, s[6], s[7], size, size);
-        int* p7 = multiply(sindices, s[8], s[9], size, size);
 
-        for (int i = 0; i < 10; ++i)
-            free(s[i]);
+        mat = malloc(sizeof(int) * size * size);
+        int* mat1 = malloc(sizeof(int) * size * size);
+
+        for (int i = 0; i < size; ++i)
+            for (int j = 0; j < size; ++j)
+                *((mat + i * size) + j) = *((a + (i + a11[0]) * s1) + j + a11[2]) + *((a + (i + a22[0]) * s1) + j + a22[2]);
+
+        for (int i = 0; i < size; ++i)
+            for (int j = 0; j < size; ++j)
+                *((mat1 + i * size) + j) = *((b + (i + a11[0]) * s2) + j + a11[2]) + *((b + (i + a22[0]) * s2) + j + a22[2]);
+
+        int* p5 = multiply(sindices, mat, mat1, size, size);
+        free(mat);
+        free(mat1);
+
+        mat = malloc(sizeof(int) * size * size);
+        mat1 = malloc(sizeof(int) * size * size);
+
+        for (int i = 0; i < size; ++i)
+            for (int j = 0; j < size; ++j)
+                *((mat + i * size) + j) = *((a + (i + a12[0]) * s1) + j + a12[2]) - *((a + (i + a22[0]) * s1) + j + a22[2]);
+
+        for (int i = 0; i < size; ++i)
+            for (int j = 0; j < size; ++j)
+                *((mat1 + i * size) + j) = *((b + (i + a21[0]) * s2) + j + a21[2]) + *((b + (i + a22[0]) * s2) + j + a22[2]);
+
+        int* p6 = multiply(sindices, mat, mat1, size, size);
+        free(mat);
+        free(mat1);
+
+        mat = malloc(sizeof(int) * size * size);
+        mat1 = malloc(sizeof(int) * size * size);
+
+        for (int i = 0; i < size; ++i)
+            for (int j = 0; j < size; ++j)
+                *((mat + i * size) + j) = *((a + (i + a11[0]) * s1) + j + a11[2]) - *((a + (i + a21[0]) * s1) + j + a21[2]);
+
+        for (int i = 0; i < size; ++i)
+            for (int j = 0; j < size; ++j)
+                *((mat1 + i * size) + j) = *((b + (i + a11[0]) * s2) + j + a11[2]) + *((b + (i + a12[0]) * s2) + j + a12[2]);
+
+        int* p7 = multiply(sindices, mat, mat1, size, size);
+        free(mat);
+        free(mat1);
+
 
         for (int i = a11[0], k = 0; i <= a11[1]; ++i, ++k)
             for (int j = a11[2], l = 0; j <= a11[3]; ++j, ++l)
