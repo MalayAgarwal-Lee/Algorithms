79f81474022e07ca1abad3f3bcc5c4b6f81be0e2
Malay Agarwal <malay.agarwal261016@outlook.com>
2020-04-15 18:50:31 +0530

    Add comments for bfs()
    Minor corrections in comments

--- breadth_first_search/bfs.c
+++ breadth_first_search/bfs.c
@@ -155,7 +155,9 @@ void initialize_graph(GRAPH* G)
         scanf("%d", &G->adj[i].size);
 
         // Allocate memory for array to store the neighbors
-        // Only the integer value of the neighbors is stored
+        // NOTE: The adjacency list stores only the integer value of the neighbors
+        // The actual vertices are only stored in G.vertices
+        // And accesses to the vertices need to access G.vertices
         G->adj[i].neighbors = malloc(sizeof(int) * G->adj[i].size);
 
         // Get integer value of all the neighbors of the vertex
@@ -223,7 +225,7 @@ void enqueue(QUEUE* q, int i)
     if (q->front == -1 && q->rear == -1)
         q->front = 0;
 
-    // rear can be incremented irrespective of whether queue is empty or note
+    // rear can be incremented irrespective of whether queue is empty or not
     q->rear++;
 
     // Assign i to the new rear of the queue
@@ -266,36 +268,82 @@ int dequeue(QUEUE* q)
 
 void bfs(GRAPH* G, int source)
 {
+    /*
+    Function to conduct a breadth-first search on G starting at vertex s
+
+    The BFS algorithm can be looked at here:
+    https://en.wikipedia.org/wiki/Breadth-first_search
+
+    Parameters:
+        G: GRAPH pointer, the graph that is to be searched
+        source: int, the starting vertex
+    */
+
+    // Get the source vertex
+    // Its color is changed to GRAY to mark that it has been discovered
+    // Since this is the source, the distance is 0
     NODE u = G->vertices[source];
     u->color = 'G';
     u->distance = 0;
 
+    // Get the vertex number for the source
     int i = u->vertex;
 
+    // Initialize the queue
+    // The queue will store the pending list of all the vertices that have been discovered
+    // But whose neighbors are yet to be discovered
+    // NOTE: The queue only stores the integer value
+    // The actual vertices are only stored in G.vertices
+    // And accesses to the vertices need to access G.vertices
     QUEUE q;
+
+    // Allocate memory for the array and initialize the front and the rear
     q.array = malloc(sizeof(int) * G->size);
     q.front = q.rear = -1;
 
+    // Add source vertex to the queue since that is the first to be discovered
     enqueue(&q, i);
 
+    // When the queue is empty, there are no more vertices left to be discovered
+    // Or that the search is complete
     while ((i = dequeue(&q)) != -1)
     {
+        // Get the actual vertex represented by i from G.vertices
         u = G->vertices[i];
 
+        // Breadth-first search proceeds by first processing all vertices at distance 1 from the source
+        // Then distance 2, distance 3 and so on
+        // So, if we are at a vertex of distance x, we need to first access its neighbors since they will be at distance x + 1 and will be processed next
         for (int j = 0; j < G->adj[i].size; ++j)
         {
+            // Get the neighbor's integer value from G.adj
             int neighbor = G->adj[i].neighbors[j];
+            // Get the actual vertex using the integer value from G.vertices
             NODE v = G->vertices[neighbor];
+
+            // If the vertex is non-white, it has already been discovered and hence, doesn't need to be processed again
             if (v->color == 'W')
             {
+                // Set the color to GRAY to indicate that the neighbor has been discovered
                 v->color = 'G';
+
+                // Set the neighbor's distance from source as +1 of the node it was discovered from
                 v->distance = u->distance + 1;
+
+                // Set the neighbor's predecessor as the node it was discovered from
                 v->predecessor = u;
+
+                // If we were at distance x, this vertex is at distance x + 1. Hence, it should be the one which is processed after all vertices at distance x are processed. Since insertion takes place at the rear of the queue, this behavior is ensured
                 enqueue(&q, v->vertex);
             }
         }
 
+        // Set color to BLACK to indicate that all the neighbors have been discovered
+        // And added to the queue
         u->color = 'B';
+
+        // Print the node and the level (distance) of the vertex
+        // Printing this node is correct since it was discovered first in the search
         printf("%d at level %d\n", u->vertex, u->distance);
     }
 }
